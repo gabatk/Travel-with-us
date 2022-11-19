f14f3e650df993cf10b2360c1d3acb5d898bcf61
gabatk <gabriela.trybuszek@gmail.com>
2022-11-19 17:21:44 +0100

    Website navigation- JS part implemented.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+
+function handleNav() {
+	nav.classList.toggle('nav--active');
+
+	allNavItems.forEach(navItem => {
+		navItem.addEventListener('click', () => {
+			nav.classList.remove('nav--active');
+		});
+	});
+}
+
+navBtn.addEventListener('click', handleNav);
