08e70fe0a6a2d258cc91b0a81bc0e322fe7d9c6d
gabatk <gabriela.trybuszek@gmail.com>
2022-11-24 17:01:42 +0100

    JavaScript functions implemented.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 function handleNav() {
 	nav.classList.toggle('nav--active');
+
+	navBtnBars.classList.remove('black-bars-color');
 
 	allNavItems.forEach(navItem => {
 		navItem.addEventListener('click', () => {
@@ -30,4 +35,28 @@ const deleteAnimation = () => {
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach(section => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
